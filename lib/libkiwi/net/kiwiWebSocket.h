03120ce2f7de2fcca2b2dccd16f74dff3ca8ed89
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2024-10-09 13:00:41 -0400

    WIP

--- lib/libkiwi/net/kiwiWebSocket.h
+++ lib/libkiwi/net/kiwiWebSocket.h
@@ -2,7 +2,6 @@
 #define LIBKIWI_NET_WEB_SOCKET_H
 #include <libkiwi/k_types.h>
 #include <libkiwi/net/kiwiAsyncSocket.h>
-#include <libkiwi/net/kiwiSyncSocket.h>
 #include <libkiwi/prim/kiwiString.h>
 
 namespace kiwi {
@@ -10,31 +9,120 @@ namespace kiwi {
 //! @{
 
 /**
- * @brief Synchronous web socket
+ * @brief WebSocket message
  */
-class SyncWebSocket : public SyncSocket {
+struct WebSockMessage {};
+
+/**
+ * @brief Web socket (asynchronous)
+ */
+class WebSocket {
+public:
+    /**
+     * @brief WebSocket result codes
+     */
+    enum EResult {
+        EResult_Success,     //!< OK
+        EResult_CantConnect, //!< Can't connect to the server
+        EResult_CantUpgrade, //!< Can't upgrade the HTTP connection
+        EResult_CantAccept,  //!< Can't authenticate server Accept field
+    };
+
+    /**
+     * @brief Generic socket operation callback
+     *
+     * @param result Socket result
+     * @param pArg User callback argument
+     */
+    typedef void (*Callback)(EResult result, void* pArg);
+
+    /**
+     * @brief Message receive callback
+     * @note If you need the message data after the callback function, make a
+     * copy of it before the function returns.
+     *
+     * @param pMessage Message data
+     * @param pArg User callback argument
+     */
+    typedef void (*MessageCallback)(const WebSockMessage* pMessage, void* pArg);
+
 public:
     /**
      * @brief Constructor
      */
-    SyncWebSocket() : SyncSocket(SO_PF_INET, SO_SOCK_STREAM) {}
+    WebSocket();
+    /**
+     * @brief Destructor
+     */
+    ~WebSocket();
 
     /**
-     * @brief Connects to a peer
+     * @brief Sets the message receive callback function
      *
-     * @param rAddr Remote address
+     * @param pCallback Callback function
+     * @param pArg Callback user argument
+     */
+    void SetMessageCallback(MessageCallback pCallback, void* pArg = nullptr) {
+        mpMessageCallback = pCallback;
+        mpMessageCallbackArg = pArg;
+    }
+
+    /**
+     * @brief Connects to the specified server hostname
+     *
+     * @param rHost Server hostname
      * @param pCallback Connection callback
      * @param pArg Callback user argument
-     * @return Success
      */
-    virtual bool Connect(const SockAddrAny& rAddr, Callback pCallback = nullptr,
-                         void* pArg = nullptr);
-};
+    void Connect(const String& rHost, Callback pCallback, void* pArg = nullptr);
+    /**
+     * @brief Connects to the specified server address
+     *
+     * @param rAddr Server address
+     * @param pCallback Connection callback
+     * @param pArg Callback user argument
+     */
+    void Connect(const SockAddrAny& rAddr, Callback pCallback,
+                 void* pArg = nullptr);
 
-/**
- * @brief Asynchronous web socket
- */
-class AsyncWebSocket : public AsyncSocket {};
+private:
+    /**
+     * @brief Asynchronous state
+     */
+    enum EState {
+        EState_None,
+        EState_Connecting,
+        EState_Receiving,
+    };
+
+private:
+    /**
+     * @brief Socket callback function
+     *
+     * @param result Socket library result
+     * @param pArg User callback argument
+     */
+    static void SocketCallbackFunc(SOResult result, void* pArg);
+
+    /**
+     * @brief Request response callback function
+     *
+     * @param rResp Request response
+     * @param pArg Callback user argument
+     */
+    static void RequestCallback(const HttpResponse& rResp, void* pArg);
+
+private:
+    EState mState;          //!< Asynchronous state
+    AsyncSocket* mpSocket;  //!< TCP socket
+    HttpRequest* mpRequest; //!< HTTP request
+
+    Callback mpConnectCallback; //!< Connect callback
+    void* mpConnectCallbackArg; //!< Connect callback user argument
+
+    MessageCallback mpMessageCallback; //!< Message receive callback
+    void* mpMessageCallbackArg;        //!< Message callback user argument
+};
 
 //! @}
 } // namespace kiwi
