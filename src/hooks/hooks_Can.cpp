5a315e68917ee630b3c77e75dadbbba9b8f36f50
plyd823 <116851074+plyd823@users.noreply.github.com>
2025-05-11 21:40:52 -0400

    Implement canoeing max timer

--- src/hooks/hooks_Can.cpp
+++ src/hooks/hooks_Can.cpp
@@ -19,25 +19,35 @@ namespace Can {
 /**
  * @brief Sets the max timer in Canoeing
  * 
- * @param pSequence Game sequence
+ * @param difficulty Difficulty level
+ * addresses for difficulty level timer setting: 0x804af188, 0x804af1e4
  * 
  */
-/*int CanSetHearts(int currentHearts) {
-    int maxHearts = ItemMgr::GetInstance().GetCanHeartNum();
-    if (currentHearts > maxHearts) {
-        currentHearts = maxHearts;
-    }
-    return currentHearts;
-}*/
+int CanSetTimer(int difficulty) {
+    // switch(difficulty) {
+    //     case 0:
+    //         return 10;
+    //     case 1: 
+    //         return 20;
+    //     case 2:
+    //         return 30;
+    //     case 3:
+    //         return 40;
+    //     case 4:
+    //         return 50;
+    // }
+
+    return 50;
+}
 
 /**
  * @brief CanSetTimer trampoline
  */
-TRAMPOLINE_DEF(0x804aafe4, 0x804aafe8) {
+TRAMPOLINE_DEF(0x804af1fc, 0x804af200) {
     // clang-format off
     TRAMPOLINE_BEGIN
 
-    li r5, 400
+    li r3, 50
 
     TRAMPOLINE_END
     blr
