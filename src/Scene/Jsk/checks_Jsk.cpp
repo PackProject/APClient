637526f250202b86d183c84f99837a8320f07dbb
Morgan Smith <msmith120304@gmail.com>
2026-01-15 00:42:28 -0700

    power cruising 1500 points check

--- src/Scene/Jsk/checks_Jsk.cpp
+++ src/Scene/Jsk/checks_Jsk.cpp
@@ -13,7 +13,26 @@ namespace Check {
  * Power Cruising
  *
  ******************************************************************************/
+namespace Std {
 
+void SetJskPtCheck(int points) {
+    if (points >= 15000) {
+        Cmn::CheckMgr::GetInstance().SetCheckState(CHECK_POWER_CRUISING_1500_POINTS, true);
+    }
+}
+
+TRAMPOLINE_DEF(0x805824ec, 0x805824f0) {
+    // clang-format off
+    TRAMPOLINE_BEGIN
+
+    add r23, r3, r24
+    mr r3, r23
+    bl SetJskPtCheck
+
+    TRAMPOLINE_END
+    blr
+}
+}
 } // namespace Check
 } // namespace Jsk
 } // namespace AP
