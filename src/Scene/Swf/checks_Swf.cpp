72cc61531624d5238b73967cf9d6ec73d5312603
Morgan Smith <msmith120304@gmail.com>
2026-03-16 21:24:33 -0600

    speed slice first win check (needs testing)

--- src/Scene/Swf/checks_Swf.cpp
+++ src/Scene/Swf/checks_Swf.cpp
@@ -10,11 +10,11 @@ namespace Check {
 
 /******************************************************************************
  *
- * Swordplay
+ * Swordplay (Duel)
  *
  ******************************************************************************/
 
-void GetSwordplayFirstWinCheck() {
+void GetDuelFirstWinCheck() {
     bool hasCheck = Cmn::CheckMgr::GetInstance().GetCheckState(
         CheckID(CHECK_SWORDPLAY_DUEL_FIRST_WIN));
 
@@ -28,7 +28,7 @@ TRAMPOLINE_DEF(0x802b1e34, 0x802b1e38) {
     // clang-format off
     TRAMPOLINE_BEGIN
 
-    bl GetSwordplayFirstWinCheck
+    bl GetDuelFirstWinCheck
     cmpwi r0, 0x1
     mfcr r0
 
@@ -36,6 +36,33 @@ TRAMPOLINE_DEF(0x802b1e34, 0x802b1e38) {
     mtcr r0
     blr
     // clang-format on
+}
+
+/******************************************************************************
+ *
+ * Swordplay (Speed Slice)
+ *
+ ******************************************************************************/
+
+void GetSpeedSliceFirstWinCheck() {
+    bool hasCheck = Cmn::CheckMgr::GetInstance().GetCheckState(
+        CheckID(CHECK_SWORDPLAY_SPEED_SLICE_FIRST_WIN));
+
+    if (!hasCheck) {
+        Cmn::CheckMgr::GetInstance().SetCheckState(
+            CheckID(CHECK_SWORDPLAY_SPEED_SLICE_FIRST_WIN), true);
+    }
+}
+
+TRAMPOLINE_DEF(0x8062f89c, 0x8062f8a0) {
+    // clang-format off
+    TRAMPOLINE_BEGIN
+
+    bl GetSpeedSliceFirstWinCheck
+
+    TRAMPOLINE_END
+    li r3, 0x2f3
+    blr
 }
 
 } // namespace Check
